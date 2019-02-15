09c578f8069f69c0722f6f28aabe13e59c783523
GarJor <jordi.ga.ag@gmail.com>
2019-02-15 23:20:11 +0100

    gumesindo first try

--- scripts/apagar_luz.sh
+++ scripts/apagar_luz.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo executant el script apagar_luz.sh...
-echo LUZ APAGADA CORRECTAMENTE
+echo 2 executant el script apagar_luz.sh...
+echo 2 LUZ APAGADA CORRECTAMENTE
---
+++ scripts/download_basic.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+if [ $# -eq 0 ]
+then
+  echo "2 Cap parametre introduit"
+  exit
+fi
+#grep -m 1 "yt-lockup-content" | tr ">" "\n" |
+SEARCH=$(echo $@ | tr " " "+")
+#echo $SEARCH grep -m 1 "yt-lockup-content"
+wget -O html --no-check-certificate "https://www.youtube.com/results?search_query=$SEARCH"
+
+awk '/yt-lockup-content/{i++}i==1' html | tr ">" "\n" | grep  "/watch?" | tr '"' "\n" | grep -m 2 -A 1 -e "title" -e "href" | grep -v -e "--" -e "title" -e "href"  > current
+
+TITLE=$(cat current | tail -n 1)
+#  LINK="https://www.easy-youtube-mp3.com/download.php?v=$(cat current | grep "/watch?" | tr "=" "\n" | tail -n 1)"
+
+echo "2 TITLE: $TITLE"
+
+
+
+wget -O html --no-check-certificate "https://www.easy-youtube-mp3.com/download.php?v=$(cat current | grep "/watch?" | tr "=" "\n" | tail -n 1)"
+echo "2 SONG: $(cat html | grep "youtubemp3api.com/@download/" | tr "\"" "\n" | grep ".mp3")"
+
+
+
+rm current html
